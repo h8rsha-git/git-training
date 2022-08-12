765fff2fc5e873891e9c87077b397c1d9723cd03
h8rsha-git <h8rsha@gmail.com>
2022-08-12 17:24:25 +0530

    Commit

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
 	public static void main(String[] args){
 		System.out.println("Test 1: Jenkins");
 		System.out.println("Test 2: Jenkins Through GitHub");
+		System.out.println("Test 3: Jenkins Through Eclipse IDE");
 	}
 }
